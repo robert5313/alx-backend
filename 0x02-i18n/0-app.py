0df8dd26c8ee715a10653b59f7cbdac71997a7e4
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-08-29 23:20:48 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-'''Module task 0.
-'''
+"""A Basic Flask app.
+"""
 from flask import Flask, render_template
 
 
@@ -13,6 +13,7 @@ def get_index() -> str:
     """The home page.
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=4000)
